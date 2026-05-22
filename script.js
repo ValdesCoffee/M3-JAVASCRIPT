fd855fc3e99603272c2caa7dbd27bc6d005be370
Auri Valdes Mendoza <valdes.2022806@ensdbexcelencia.edu.co>
2026-05-21 21:55:48 -0500

    task 2

---
+++ script.js
@@ -0,0 +1,7 @@
+function leerTexto() {
+
+  const input1 = document.getElementById('input-1')
+  const lista = document.getElementById('lista')
+
+  const texto = input1.value.trim()
+  console.log(lista)
