5081c6a075ef9a216df86566ba7ddd20397b661c
Auri Valdes Mendoza <valdes.2022806@ensdbexcelencia.edu.co>
2026-05-21 22:01:57 -0500

    TASK 4 Y TASK 5
    las dos se relacionan mucho asi que da igual

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const notas = []
 function leerTexto() {
 
   const input1 = document.getElementById('input-1')
   const lista = document.getElementById('lista')
 
   const texto = input1.value.trim()
-  console.log(lista)
-if (texto !== "") {
-li.textContent = texto
+
+  if (texto !== "") {
+    const li = document.createElement("li")
+
+    li.textContent = texto
+
+    lista.appendChild(li)
+
+    input1.value = ''
+
+  }
+
 }
